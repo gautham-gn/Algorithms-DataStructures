8099bca80ee23c23b03af9a429bd6a503b1e1117
Nagendra Gautham Gondi <gautham.gn@gmail.com>
2020-11-04 13:57:44 -0500

    Added Hash Table Data Structure

--- JavaScript/Data Structures/Hash Maps/HashTable.js
+++ JavaScript/Data Structures/Hash Maps/HashTable.js
@@ -16,12 +16,10 @@ class HashTable {
 
     set(key, value) {
         let hash = this._hash(key);
-        if (this.keyMap[hash]) {
-            this.keyMap[hash].push([key, value]);
+        if (!this.keyMap[hash]) {
+            this.keyMap[hash] = [];
         }
-        else {
-            this.keyMap[hash] = [[key, value]];
-        }
+        this.keyMap[hash].push([key, value]);
         return this.keyMap;
     }
 
@@ -36,6 +34,34 @@ class HashTable {
         }
         return;
     }
+
+    keys() {
+        let keysArr = [];
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (let j = 0; j < this.keyMap[i].length; j++) {
+                    if (!keysArr.push(this.keyMap[i][j][0])) {
+                        keysArr.push(this.keyMap[i][j][0]);
+                    }
+                }
+            }
+        }
+        return keysArr;
+    }
+
+    values() {
+        let valuesArr = [];
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (let j = 0; j < this.keyMap[i].length; j++) {
+                    if (!valuesArr.includes(this.keyMap[i][j][1])) {
+                        valuesArr.push(this.keyMap[i][j][1]);
+                    }
+                }
+            }
+        }
+        return valuesArr;
+    }
 }
 
 let map = new HashTable(13);
@@ -47,6 +73,7 @@ console.log(map.set("black", "#6fc092"));
 console.log(map.set("white", "#a69c5v"));
 console.log(map.set("orange", "#ffe354"));
 console.log(map.set("yellow", "#00ff5c"));
+console.log(map.set("maroon", "#00ff5c"));
 console.log(map.get("green"));
 console.log(map.get("pink"));
 console.log(map.get("orange"));
@@ -54,3 +81,5 @@ console.log(map.get("cyan"));
 console.log(map.get("blue"));
 console.log(map.get("yellow"));
 console.log(map.get("sky"));
+console.log(map.keys());
+console.log(map.values());
