72934c1d6bba2d4e0f939c6b90ab5e06d55111e5
Gautham Nagendra <gautham.gn@gmail.com>
2021-01-04 10:08:32 -0500

    Added String Search

--- Python/Searching Algorithms/naiveStringSearch.py
+++ Python/Searching Algorithms/naiveStringSearch.py
@@ -1,12 +1,25 @@
 def naiveStringSearch(str, pattern):
     counter = 0
+    for i in range(0,len(str)):
+        for j in range(0, len(pattern)):
+            if pattern[j] != str[i+j]:
+                break
+            if j == len(pattern) - 1:
+                counter += 1
+    return counter
+
+def stringSearch(str, pattern):
+    counter = 0
     pattern_length = len(pattern)
     for i in range(0,len(str) - pattern_length + 1):
         if str[i:i+pattern_length] == pattern:
-            print(str[i:i+pattern_length])
             counter += 1
     return counter
 
 print(naiveStringSearch("hahaha I am a haha  haha  haha boy","haha"))
 print(naiveStringSearch("hahahahahaha I am a boy","haha"))
 print(naiveStringSearch("hahaha I am a haha boy","boy"))
+
+print(stringSearch("hahaha I am a haha  haha  haha boy","haha"))
+print(stringSearch("hahahahahaha I am a boy","haha"))
+print(stringSearch("hahaha I am a haha boy","boy"))
